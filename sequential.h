6224dfa42f5a98bc85262bf91dd2959657343c16
Niels J. de Wit <ndewit@gmail.com>
2018-07-20 12:23:17 +0200

    fixed sequential.h (soon deprecated) for OSX (Posix)

--- sequential.h
+++ sequential.h
@@ -9,18 +9,40 @@
 
 #pragma once
 
+#include <stdlib.h>
+
+// FIXME: move elsewhere or pick a different allocator for the entire project.
+//        Also a bit odd that both Windows and OSX don't implement aligned_alloc() (C++11).
+#ifdef _WIN32
+
+	inline void* AllocAligned(size_t size, size_t alignment) { return _aligned_malloc(size, alignment); }
+	inline void FreeAligned(void* address) { _aligned_free(address); }
+
+#else // _GNUC_, hopefully
+
+	inline void* AllocAligned(size_t size, size_t alignment) 
+	{ 
+		void* address;
+		posix_memalign(&address, alignment, size);
+		return address;
+	}
+
+	inline void FreeAligned(void* address) { free(address); }
+
+#endif
+
 template <typename T> class SeqAlloc
 {
 public:
 	SeqAlloc(size_t count) :
 		m_index(0)
 	{
-		m_blocks = static_cast<T*>(_aligned_malloc(count*sizeof(T), sizeof(size_t)<<3) /* Likely cache line size */);
+		m_blocks = static_cast<T*>(AllocAligned(count*sizeof(T), sizeof(size_t)<<3) /* Likely cache line size */);
 	}
 
 	~SeqAlloc()
 	{
-		_aligned_free(m_blocks);
+		FreeAligned(m_blocks);
 	}
 
 	inline T* New()
